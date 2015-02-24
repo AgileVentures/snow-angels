fc9091d274edc966a320e7bf8e3eb58b03a2d6ed
bibiana <bcristofol@gmail.com>
2015-02-24 15:13:00 +0000

    Add Home link

--- spec/features/pages_feature_spec.rb
+++ spec/features/pages_feature_spec.rb
@@ -13,10 +13,18 @@ feature 'pages' do
     end
 
     scenario "should display the 'sign up', 'add client', and 'View client' links" do
-      visit '/pages'
+      visit '/'
       expect(page).to have_link 'Sign up'
       expect(page).to have_link 'Add client'
       expect(page).to have_link 'View client'
+      expect(page).to have_link 'Home'
+    end
+
+    it 'should be able to return to homepage' do
+      visit '/'
+      click_link 'View client'
+      click_link 'Home'
+      expect(current_path).to eq '/pages'
     end
 
   end
