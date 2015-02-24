eb7ebb39848d26f119d2960ff3a238d8c9638f40
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-24 14:24:14 +0000

    Changes to homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :pages
   resources :clients
-  root to: "home#index"
+  root to: "pages#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
