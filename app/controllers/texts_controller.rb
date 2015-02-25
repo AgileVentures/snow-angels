aa794f5d5ca4579da19d73aa5acae1e20770a3ae
bibiana <bcristofol@gmail.com>
2015-02-25 16:26:20 +0000

    Able to send sms to a volunteer hash

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -21,19 +21,4 @@ class TextsController < ApplicationController
     @unavailable = Volunteer.where(availability: false)
   end
 
-  # def client
-  #   Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
-  # end
-
-  # def send_text
-  #   client.messages.create(
-  #     from: ENV['TWILIO_NUMBER']
-  #     to: ENV['PHONE_NO']
-  #     body: message_body)
-  # end
-
-  # def message_body
-  #   "It's snowing! Are you available to help today?"
-  # end
-
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -2,11 +2,17 @@ class Text < ActiveRecord::Base
   belongs_to :volunteer
 
   def send_text
+    volunteers = {
+      "+44" => "Bibiana",
+      "+44" => "Steph"
+    }
     client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
     account = client.account
-    message = account.messages.create({:body => "It's snowing! Are you available to help today?",
-          :to => ENV['PHONE_NO'],
-          :from => ENV['TWILIO_NO']})
-    puts message
+    volunteers.each do |key, value|
+      message = account.messages.create({:body => "It's snowing! #{value} are you available to help today?",
+            :to => key,
+            :from => ENV['TWILIO_NO']})
+      puts message
+    end
   end
 end
