96f6dfd76a8be0db83ab12efed8e268da0cf85c0
bibiana <bcristofol@gmail.com>
2015-02-25 10:38:40 +0000

    Show sms linked with volunteer

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,11 +3,16 @@ class SmsController < ApplicationController
 
   def create
     @sms = Sms.new
+    
     @sms.number = params['From']
     @sms.body = params['Body']
+
+    @sms.volunteer = Volunteer.find_by(mobile_number: params['From'])
+    
     if @sms.save
       render xml: "<Response/>"
     end
+
   end
 
   def index
