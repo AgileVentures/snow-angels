d90e4d487b129d09dc57c4c80c496d9d1d237d55
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-27 17:29:44 +0000

    Adds DBS check for a volunteer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227144301) do
+ActiveRecord::Schema.define(version: 20150227172329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20150227144301) do
     t.boolean  "grit_spreading"
     t.boolean  "dog_walking"
     t.string   "email"
+    t.boolean  "dbs"
   end
 
   add_foreign_key "tasks", "clients"
