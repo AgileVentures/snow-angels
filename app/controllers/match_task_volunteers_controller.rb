0d341d86e8ae885a1308e38ce0abb63b0029ceeb
bibiana <bcristofol@gmail.com>
2015-03-02 10:52:51 +0000

    Fix bug with matching

--- app/controllers/match_task_volunteers_controller.rb
+++ app/controllers/match_task_volunteers_controller.rb
@@ -3,13 +3,13 @@ class MatchTaskVolunteersController < ApplicationController
   def match
     @task = Task.find(params[:id])
     @available = Volunteer.where(availability: true)
-    @available_dbs = Volunteer.where(availability: true, dbs: true)
-    if @available_dbs.any?
-      @match = MatchTaskVolunteer.create(volunteer_id: @available_dbs.first.id, task_id: @task.id)
-      @available_dbs.first.update(availability: false)
-      if @available_dbs.any?
-        @match_two = MatchTaskVolunteer.create(volunteer_id: @available_dbs.first.id, task_id: @task.id)
-        @available_dbs.first.update(availability: false)
+    @dbs = Volunteer.where(dbs: true)
+    if @available.any? && @dbs.any?
+      @match = MatchTaskVolunteer.create(volunteer_id: @dbs.first.id, task_id: @task.id)
+      @dbs.first.update(availability: false)
+      if @available.any? && @dbs.any?
+        @match_two = MatchTaskVolunteer.create(volunteer_id: @dbs.second.id, task_id: @task.id)
+        @dbs.second.update(availability: false)
       elsif @available.any?
         @match_two = MatchTaskVolunteer.create(volunteer_id: @available.first.id, task_id: @task.id)
         @available.first.update(availability: false)
@@ -20,8 +20,8 @@ class MatchTaskVolunteersController < ApplicationController
       @match = MatchTaskVolunteer.create(volunteer_id: @available.first.id, task_id: @task.id)
       @available.first.update(availability: false)
       if @available.any?
-        @match_two = MatchTaskVolunteer.create(volunteer_id: @available.first.id, task_id: @task.id)
-        @available.first.update(availability: false)
+        @match_two = MatchTaskVolunteer.create(volunteer_id: @available.second.id, task_id: @task.id)
+        @available.second.update(availability: false)
       else
         flash[:notice] = "There are no more unmatched volunteers"
       end
