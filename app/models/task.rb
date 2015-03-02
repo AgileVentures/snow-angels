99911aed12266acd4b0ed3b70faefe7544433851
stepholdcorn <stephieoldcorn@gmail.com>
2015-03-02 21:09:56 +0000

    Links from the client name to the summary page

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,6 @@ class Task < ActiveRecord::Base
   has_many :volunteers, through: :match_task_volunteer
   belongs_to :client
 
-
   def geocoder_lat(post_code)
     location = Geocoder.search(post_code)
     location[0].latitude
