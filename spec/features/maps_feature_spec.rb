948f92d3460483f1c65bc85c8c4619db4cf1ea46
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2015-03-02 14:22:23 +0000

    Added maps functionality markers

---
+++ spec/features/maps_feature_spec.rb
@@ -0,0 +1,13 @@
+feature 'maps' do
+
+  context 'Task Summary' do
+
+    before { admin_sign_in }
+
+    scenario 'Show Summary Page' do
+      click_link 'Great Dame'
+      expect(page).to have_content 'Task Summary'
+      expect(page).to have_content 'Map Summary'
+    end
+  end
+end
