753c759066d3ec4dd9f7d66352c390aff337cfc8
stepholdcorn <stephieoldcorn@gmail.com>
2015-03-02 11:12:30 +0000

    Adds a third matched volunteer

--- app/controllers/match_task_volunteers_controller.rb
+++ app/controllers/match_task_volunteers_controller.rb
@@ -4,27 +4,51 @@ class MatchTaskVolunteersController < ApplicationController
     @task = Task.find(params[:id])
     @available = Volunteer.where(availability: true)
     @dbs = Volunteer.where(dbs: true)
+
     if @available.any? && @dbs.any?
       @match = MatchTaskVolunteer.create(volunteer_id: @dbs.first.id, task_id: @task.id)
       @dbs.first.update(availability: false)
-      if @available.any? && @dbs.any?
+
+      if @available.any? && @dbs.second
         @match_two = MatchTaskVolunteer.create(volunteer_id: @dbs.second.id, task_id: @task.id)
         @dbs.second.update(availability: false)
+
+        if @available.any? && @dbs.third
+          @match_three = MatchTaskVolunteer.create(volunteer_id: @dbs.third.id, task_id: @task.id)
+          @dbs.third.update(availability: false)
+        elsif @available.any?
+          @match_three = MatchTaskVolunteer.create(volunteer_id: @available.first.id, task_id: @task.id)
+          @available.first.update(availability: false)
+        else
+          flash[:notice] = "There are no more unmatched volunteers"
+        end
+
       elsif @available.any?
         @match_two = MatchTaskVolunteer.create(volunteer_id: @available.first.id, task_id: @task.id)
         @available.first.update(availability: false)
       else
         flash[:notice] = "There are no more unmatched volunteers"
       end
+
     elsif @available.any?
       @match = MatchTaskVolunteer.create(volunteer_id: @available.first.id, task_id: @task.id)
       @available.first.update(availability: false)
+
       if @available.any?
         @match_two = MatchTaskVolunteer.create(volunteer_id: @available.second.id, task_id: @task.id)
         @available.second.update(availability: false)
+
+        if @available.any?
+          @match_three = MatchTaskVolunteer.create(volunteer_id: @available.third.id, task_id: @task.id)
+          @available.third.update(availability: false)
+        else
+          flash[:notice] = "There are no more unmatched volunteers"
+        end
+
       else
         flash[:notice] = "There are no more unmatched volunteers"
       end
+
     else
       flash[:notice] = "There are no more unmatched volunteers"
     end
