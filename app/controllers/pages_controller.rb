2fe6b83788f440e9b104b86880fb367b463791a6
Josh Bebbington <josh@focalwebsites.com>
2015-03-03 13:05:42 +0000

    Refactoring matching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
   def index
     @available = Volunteer.where(availability: true)
     @tasks_to_be_assigned = Task.to_be_assigned
-    @completed_tasks = Task.done
     @assigned_tasks = Task.assigned
+    @completed_tasks = Task.done
     @matches = MatchTaskVolunteer.all
   end
 
