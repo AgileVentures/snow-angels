18fae806bebccf6dc9d634b756d4502334932cea
Josh Bebbington <josh@focalwebsites.com>
2015-03-03 12:30:05 +0000

    All the refactoring

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,10 @@ class Task < ActiveRecord::Base
   has_many :volunteers, through: :match_task_volunteer
   belongs_to :client
 
+  scope :to_be_assigned, -> { where(task_done: nil, volunteer_id: nil) }
+  scope :done, -> { where task_done: true }
+  scope :assigned, -> { where "volunteer_id IS NOT NULL AND task_done IS NULL" }
+
   def geocoder_lat(post_code)
     location = Geocoder.search(post_code)
     location[0].latitude
