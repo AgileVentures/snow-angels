c71c854369cd32da7010142c306310e3806f5338
bibiana <bcristofol@gmail.com>
2015-03-03 16:56:00 +0000

    Postcode validation

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -3,7 +3,7 @@ class Volunteer < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :mobile_number
   validates_presence_of :last_name
-  validates :post_code, :postcode => true
+  validates_presence_of :post_code
 
 
 
