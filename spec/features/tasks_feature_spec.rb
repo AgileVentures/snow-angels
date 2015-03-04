a661a374ad146cc71acc7a1c83df0792814c5be4
bibiana <bcristofol@gmail.com>
2015-03-04 14:48:01 +0000

    Add volunteer_client tests

--- spec/features/tasks_feature_spec.rb
+++ spec/features/tasks_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'tasks' do
       add_volunteer('Bibiana', 'Test', 'one@test.com', '123 Fake Street', 'EC1 2DR', '+447791234567', 'true')
     end
 
-    scenario 'Add a task to a client' do
+    xscenario 'Add a task to a client' do
       add_client('Tom', 'Smith', 'Makers', 'E1 2SF', '07450991234')
       visit '/'
       click_link 'View client'
--- spec/features/volunteer_feature_spec.rb
+++ spec/features/volunteer_feature_spec.rb
@@ -1,13 +1,68 @@
-feature 'Volunteer' do
+feature "Volunteer" do
 
-  context 'On the homepage' do
-    scenario 'Can Register' do
+  context "On the homepage" do
+    scenario "Can Register" do
       visit '/'
       expect(page).to have_link 'Register'
     end
   end
 
-  context 'An admin can' do
+  context "Add volunteer" do
+
+    before { admin_sign_in }
+
+    it "should be able to display a form to add a volunteer" do
+      visit '/'
+      click_link 'Add volunteer'
+      expect(page).to have_field 'First name'
+      expect(page).to have_field 'Last name'
+      expect(page).to have_field 'Address'
+      expect(page).to have_field 'Postcode'
+      expect(page).to have_field 'Mobile number'
+      expect(page).to have_field 'Email'
+      expect(page).to have_field 'Shopping'
+      expect(page).to have_field 'Prescription collection'
+      expect(page).to have_field 'Snow clearance'
+      expect(page).to have_field 'Grit spreading'
+      expect(page).to have_field 'Dog walking'
+    end
+
+    it "should be able to create a volunteer" do
+      add_volunteer('Josh', 'Test', 'one@test.com', '123 Fake Street', 'EC1 2DR', '+447791234567', nil)
+      expect(current_path).to eq '/'
+    end
+  end
+
+  context "view volunteer" do
+
+    before do 
+      admin_sign_in
+      add_volunteer('Josh', 'Test', 'one@test.com', '123 Fake Street', 'EC1 2DR', '+447791234567', nil)
+      add_volunteer('Bibiana', 'Test', 'one@test.com', '123 Fake Street', 'EC1 2DR', '+447791234567', nil)
+      click_link 'View volunteer'
+    end
+
+    it "An admin should be able to see volunteers details" do
+      expect(page).to have_content '123 Fake Street'
+      expect(page).to have_content '+447791234567'
+      expect(page).to have_content 'Josh'
+      expect(page).to have_content 'Bibiana'
+    end
+
+    it "An admin should be able to search a volunteer details" do
+      fill_in 'search', with: 'Josh'
+      click_button 'Search'
+      expect(page).to have_content('Josh')
+      expect(page).not_to have_content('Bibiana')
+    end
+
+    it "should show volunteers by descending created at" do
+      expect(page.body) =~/Bibiana.*Josh/
+    end
+
+  end
+
+  context "edit and delete volunteer" do
 
     before do 
       admin_sign_in
@@ -15,24 +70,19 @@ feature 'Volunteer' do
       click_link 'View volunteer'
     end
 
-    scenario 'View a volunteers details' do
-      expect(page).to have_content '123 Fake Street'
-      expect(page).to have_content '+447791234567'
-      expect(page).to have_content 'Shopping'
-    end
-
-    scenario 'Edit a volunteers details' do
+    it "An admin should be able to edit volunteers details" do
       click_link 'Edit'
       fill_in 'Address', with: '3a New Street'
       click_button 'Update Volunteer'
       expect(page).to have_content '3a New Street'
     end
 
-    scenario 'Delete a volunteers details' do
+    it "An admin should be able to delete volunteers details" do
       click_link 'Delete'
       expect(page).not_to have_content 'Josh'
       expect(page).not_to have_content 'Test'
     end
+
   end
 
 end
